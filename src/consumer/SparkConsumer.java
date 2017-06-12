250d9ec144445263b32756692b612e986ade8f41
sirdan13 <lombardidaniele@hotmail.com>
2017-06-12 19:30:28 +0200

    Look and Feel fixed
    A better-looking LookandFeel for the popup-messages.

--- src/consumer/SparkConsumer.java
+++ src/consumer/SparkConsumer.java
@@ -1,5 +1,10 @@
 package consumer;
 
+import java.awt.Color;
+import javax.swing.plaf.metal.MetalLookAndFeel;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.Point;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Map;
@@ -25,7 +30,14 @@ import scala.Tuple2;
 
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
+import javax.swing.JDialog;
+import javax.swing.JLabel;
 import javax.swing.JOptionPane;
+import javax.swing.SwingConstants;
+import javax.swing.UIManager;
+import javax.swing.UIManager.LookAndFeelInfo;
+import javax.swing.UnsupportedLookAndFeelException;
+import javax.swing.plaf.ColorUIResource;
 public class SparkConsumer {
 	
 	static Icon icon = new ImageIcon("config/icon.png");
@@ -37,67 +49,36 @@ public class SparkConsumer {
 	static String kafka_consumer_group = "";
 	static String threads = "";
 	static long duration;
-	static String topic;
+	static String topic = "";
 	static Map<String, Integer> topics;
 	static JavaPairDStream<String, String> messages;
 	static JavaPairDStream<String, Integer> lines;
 	
-	static Function2<Integer, Integer, Integer> sumFunc = new Function2<Integer, Integer, Integer>() {
-
-		private static final long serialVersionUID = 1L;
-
-		@Override public Integer call(Integer i1, Integer i2) throws Exception {
-	  	    return i1 + i2;
-	  	  }
-	  	};
-	  	
-	static Function<JavaPairRDD<Integer,String>, JavaPairRDD<Integer, String>> sortFunc = new Function<JavaPairRDD<Integer,String>, JavaPairRDD<Integer,String>>() {
-        
-		private static final long serialVersionUID = 1L;
-
-		@Override
-        public JavaPairRDD<Integer,String> call(JavaPairRDD<Integer,String> rdd) throws Exception {
-                   return rdd.sortByKey(false);
-                 }
-             };
-           
-     static FlatMapFunction<Tuple2<String, String>, String> wordFunc = new FlatMapFunction<Tuple2<String, String>, String>(){
-    	 private static final long serialVersionUID = 1L;
-
- 		@Override
-         public Iterator<String> call(Tuple2<String,String> x) throws Exception {
-                    List<String> output = new ArrayList<String>();
-                    if(x._2.length()==0)
-                    	return output.iterator();
-                    for(String w : x._2().split(" "))
-                    	output.add(w);
-                    return output.iterator();
-                    }
-
-		
-        };
-     
-	  	
+	
 	 
-
-	public static void main(String[] args) throws FileNotFoundException, InterruptedException {
+     public static void main(String[] args) throws FileNotFoundException, InterruptedException, ClassNotFoundException, InstantiationException, IllegalAccessException, UnsupportedLookAndFeelException {
 		
-		String [] options = {"hashtags", "mentions", "original-text", "processed-text"};
-		topic = (String) JOptionPane.showInputDialog(null, "Scegli un valore dalla lista", "Topic", 3, icon, options, options[0]);
-		if(topic==null){
-			JOptionPane.showMessageDialog(null, "Nessun topic selezionato", "Topic", 0, icon);
-			System.exit(-1);
+    	 for (LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
+ 	        if ("Windows".equals(info.getName())) {
+ 	            UIManager.setLookAndFeel(info.getClassName());
+ 	            break;
+ 	        }
+ 	    }
+    	 
+    	chooseTopic();
+		while(topic==null){
+			if(noTopicChosen()==0){
+				if(chooseTopic()!=null)
+					break;
+			}
+			else
+				System.exit(-1);
 		}
 			
 		loadProperties();
 		init();
 		analyzeTopic(topic);
 
-		/*
-		 * Da qui ramificare l'esecuzione. A seconda del topic scelto, verrà chiamato il corrispondente metodo di analisi.
-		 */
-		
-		
 		
 	}
 	
@@ -131,6 +112,33 @@ public class SparkConsumer {
 		sc.close();
 
 	}
+	
+	private static String chooseTopic(){
+		 String [] options = {"hashtags", "mentions", "original-text", "processed-text"};
+		 UIManager.put("OptionPane.background",new ColorUIResource(255,255,204));
+		 UIManager.put("Panel.background",new ColorUIResource(255,255,204));
+		 UIManager.put("OptionPane.messageFont", "Verdana");
+		 UIManager.put("OptionPane.messageForeground", Color.green);
+		 Dimension size = UIManager.getDimension("OptionPane.minimumSize");
+		 size.width = 350;
+		 size.height= 220;
+		 UIManager.put("OptionPane.minimumSize", size);
+		 JLabel label = new JLabel("Scegli un topic:");
+		 label.setFont(new Font("Calibri", Font.BOLD, 20));
+		 label.setHorizontalAlignment(SwingConstants.LEFT);
+		 label.setVerticalAlignment(SwingConstants.CENTER);
+		 topic = (String) JOptionPane.showInputDialog(null, label, "Topic", 3, icon, options, label);
+		 return topic;
+	}
+	
+	private static int noTopicChosen() throws ClassNotFoundException, InstantiationException, IllegalAccessException, UnsupportedLookAndFeelException{
+		JLabel label2 = new JLabel("<html>Nessun topic scelto.<br>Riprovare?</html>");
+		label2.setFont(new Font("Calibri", Font.BOLD, 20));
+		label2.setHorizontalAlignment(SwingConstants.LEFT);
+		label2.setVerticalAlignment(SwingConstants.CENTER);
+		String [] errorOptions = {"Riprova", "Esci"};
+		return JOptionPane.showOptionDialog(null, label2, "Topic", 0, 0, icon, errorOptions, "Riprova");
+		}
 	
 	
 	private static void analyzeTopic(String topic) throws InterruptedException{
@@ -181,5 +189,42 @@ public class SparkConsumer {
 		jssc.start();
 		jssc.awaitTermination();
 	}
+	
+	
+	static Function2<Integer, Integer, Integer> sumFunc = new Function2<Integer, Integer, Integer>() {
+
+		private static final long serialVersionUID = 1L;
+
+		@Override public Integer call(Integer i1, Integer i2) throws Exception {
+	  	    return i1 + i2;
+	  	  }
+	  	};
+	  	
+	static Function<JavaPairRDD<Integer,String>, JavaPairRDD<Integer, String>> sortFunc = new Function<JavaPairRDD<Integer,String>, JavaPairRDD<Integer,String>>() {
+        
+		private static final long serialVersionUID = 1L;
+
+		@Override
+        public JavaPairRDD<Integer,String> call(JavaPairRDD<Integer,String> rdd) throws Exception {
+                   return rdd.sortByKey(false);
+                 }
+             };
+           
+     static FlatMapFunction<Tuple2<String, String>, String> wordFunc = new FlatMapFunction<Tuple2<String, String>, String>(){
+    	 private static final long serialVersionUID = 1L;
+
+ 		@Override
+         public Iterator<String> call(Tuple2<String,String> x) throws Exception {
+                    List<String> output = new ArrayList<String>();
+                    if(x._2.length()==0)
+                    	return output.iterator();
+                    for(String w : x._2().split(" "))
+                    	output.add(w);
+                    return output.iterator();
+                    }
+
+		
+        };
+     
 
 }
